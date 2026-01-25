519174e63d8bfe61b6761feb7eea4efab6567f36
EmirSnmezz <emircan_snmez@outlook.com>
2026-01-25 04:20:06 +0300

    product model created

---
+++ Models/EntityModels/ProductModel.cs
@@ -0,0 +1,9 @@
+public class ProductModel : Model, IProductModel
+{
+    public ImageModel Image { get; set; }
+    public string ImageId { get; set; }
+    public ProductCategoryModel Category { get; set; }
+    public string CategoryId { get; set; }
+    public string ProductTitle { get; set; }
+    public string ProductDescription { get; set; }
+}
