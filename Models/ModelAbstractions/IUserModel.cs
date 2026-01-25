e9d9203c71139f5ac709c7ad740b2342d39a82ad
EmirSnmezz <emircan_snmez@outlook.com>
2026-01-25 04:17:24 +0300

    user model interface created

---
+++ Models/ModelAbstractions/IUserModel.cs
@@ -0,0 +1,8 @@
+public interface IUserModel : IModel
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Username { get; set; }
+}
