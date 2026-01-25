5a9a90b7d98573a3d9814789e631251ed903cf2d
EmirSnmezz <emircan_snmez@outlook.com>
2026-01-25 04:19:21 +0300

    user model created

---
+++ Models/EntityModels/User.cs
@@ -0,0 +1,8 @@
+public class User : Model, IUserModel
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Username { get; set; }
+}
