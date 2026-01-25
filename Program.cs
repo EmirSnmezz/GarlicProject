02e48d4e2b13534ba11f38018f28f3f94e0fe0cc
EmirSnmezz <emircan_snmez@outlook.com>
2026-01-25 04:07:25 +0300

    Service registration added for dal and service classes, add map controller for routing

--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddTransient<ISliderContentDal, SliderContentDal>();
 
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUserDal, UserDal>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddTransient<ISliderDal, SliderDal>();
+
+builder.Services.AddScoped<ISliderService, SliderService>();
+
+builder.Services.AddScoped<IProductDal, ProductDal>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<IProductCategoryModelService, ProductCategoryService>();
+
+builder.Services.AddScoped<IProductCategoryDal, ProductCategoryModelDal>();
+
 
 var app = builder.Build();
 
@@ -20,6 +39,8 @@ app.UseAuthorization();
 
 app.UseStaticFiles();
 
-app.MapControllerRoute(name:"default", pattern:"{controller=Home}/{action=Index}");
+app.MapControllerRoute(name:"default", pattern:"{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllers();
 
 app.Run();
