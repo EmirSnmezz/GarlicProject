1973ba2984ce7f84332f715e0da83a15455f5597
EmirSnmezz <emircan_snmez@outlook.com>
2026-02-05 09:41:55 +0300

    Authentication Controller Created

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,22 +1,67 @@
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Mvc;
 
-[ApiController]
-[Route("api/[controller]")]
-public class AuthenticationController: ControllerBase
+
+[Route("Authentication")]
+public class AuthenticationController: Controller
 {
-    [HttpPost("createToken")]
-    public IActionResult CreateAccessToken()
+
+    IAuthService _authService;
+    IUserService _userService;
+
+    public AuthenticationController(IAuthService authService, IUserService userService)
     {
-        JwtSecurityToken token = new JwtSecurityToken(
-            issuer:"emir",
-            audience:"emir",
-            expires:DateTime.UtcNow.AddHours(1),
-            signingCredentials: SigningCredentialHelper.CreateSigningCredential(SecurityKeyHelper.CreateSecurityKey("mySuperSecretSecurityKeysuuuu43y43245y237543"))
-        )   ;
+        _authService = authService;
+        _userService = userService;
+    }
 
-        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+    [HttpGet("Login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
 
-        return Ok(handler.WriteToken(token));
+    [HttpPost("Login")]
+    public IActionResult Login(LoginModelDTO user)
+    {
+        var isAuth = _authService.Login(user);
+        
+        if(isAuth.IsSuccess)
+        {
+           var userInDb = _userService.GetAll(null).Data.FirstOrDefault(x => x.Username == user.UserName);
+           var token = _authService.CreateAccessToken(userInDb);
+           
+ Response.Cookies.Append("jwtToken", token.Token, new CookieOptions
+{
+    HttpOnly = true,
+    Secure = false, // Localhost/HTTP çalıştığın için false kalmalı
+    SameSite = SameSiteMode.Lax, // Strict yerine Lax yap
+    Expires = token.Expiration,
+    Path = "/"
+});
+
+
+    return RedirectToAction("Index", "Admin");
+        }
+
+        return BadRequest(isAuth.Message);
+        
+    }
+
+     [HttpPost("Register")]
+    public IActionResult Register([FromBody] RegisterModelDTO user)
+    {
+        var isThereUser = _userService.GetUserByEmailOrUserName(user.Username);
+
+        if(!isThereUser.IsSuccess)
+        {
+           var registerUser = _authService.Register(user);
+           if(registerUser.IsSuccess)
+            return Ok(registerUser.IsSuccess);
+        }
+
+        return BadRequest();
+        
     }
 }
