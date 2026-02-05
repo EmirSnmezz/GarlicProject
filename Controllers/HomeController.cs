e22cd12e400b4ecf93f8b12f2adb7e89a94ace45
EmirSnmezz <emircan_snmez@outlook.com>
2026-02-05 09:44:29 +0300

    Contact Form Created and written view forms in admin panel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,9 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 
 public class HomeController: Controller
 {
+    IProductService _productService;
+    IImageService _imageService;
+    ISliderService _sliderService;
+    IFormService _formService;
+    public HomeController(IProductService productService, IImageService imageService, ISliderService sliderService, IFormService formService)
+    {
+        _productService = productService;
+        _imageService = imageService;
+        _sliderService = sliderService;
+        _formService = formService;
+    }
     public IActionResult Index()
     {
-        return View();
+        List<SliderModel> sliders = _sliderService.GetAll().Data;
+        List<ImageModel> images = _imageService.GetAll(x => x.SliderId != null).Data;
+        ViewBag.images = images;
+        return View(sliders);
     }
 
     public IActionResult AboutUs()
@@ -14,11 +28,22 @@ public class HomeController: Controller
 
     public IActionResult Products()
     {
-        return View();
+        var result = _productService.GetAll().Data;
+        var imageResult = _imageService.GetAll(null).Data;
+        return View(result);
     }
 
     public IActionResult Contact()
     {
         return View();
+    }
+
+    public IActionResult CreateOrder(FormModel formModel)
+    {
+        var result = _formService.Add(formModel);
+        if(result.IsSuccess)
+            return RedirectToAction("Contact");
+
+        return BadRequest();
     }
 }
