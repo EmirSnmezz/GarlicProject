51655ed189e44cc656f6beb67d3f862c059031b3
EmirSnmezz <emircan_snmez@outlook.com>
2026-02-05 10:03:58 +0300

    Login and Register Models Created for User operations and Authentication

---
+++ Models/DTOS/RegisterModelDTO.cs
@@ -0,0 +1,8 @@
+public class RegisterModelDTO
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
